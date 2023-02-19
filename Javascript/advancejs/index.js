77105036280ac9f1ff36d704ff4fbc168d66123b
Shridhar-Atram <shridharatram555@gmail.com>
2023-02-19 21:45:51 +0530

    New Files added

---
+++ Javascript/advancejs/index.js
@@ -0,0 +1,15 @@
+function sum(x,y){
+	return x+y;
+}
+
+function mul(x,y){
+	return x*y;
+}
+
+function calculator(m,n,operator)
+{
+return operator(m,n);
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+function lw()
+{
+alert("Shridhar");
+}
+
+lw()
